6fb9bd249f7f66be7d878ff1c4d160692cb9ba77
Zhang-ZhaoHui <2310768059@qq.com>
2023-09-18 22:27:36 +0800

    创建项目，静态页面

---
+++ pages/location/location.vue
@@ -0,0 +1,96 @@
+<template>
+    <view class="content">
+      <map style="width: 100%;height:100vh"
+		:show-location='true'  
+	  :latitude="latitude" :longitude="longitude" :markers="marker" 
+	  :scale="scale"  @markertap="markertap"   @callouttap='callouttap'
+	  >
+       </map>
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+				latitude:39.110388, //纬度
+				longitude:117.196269,//经度
+				scale:13,//缩放级别
+				bottomData:false,
+				marker: [
+					{
+						id:0,
+						latitude: 39.110388,//纬度
+						longitude: 117.196269,//经度
+						iconPath: '',    //显示的图标  
+						width:20,   //宽
+						height:30,   //高
+						//   title:'我在这里',//标注点名
+						alpha:0.7,   //透明度
+						callout:{//自定义标记点上方的气泡窗口 点击有效  
+							content:'五大道',//文本
+							color:'#ffffff',//文字颜色
+							fontSize:14,//文本大小
+							borderRadius:15,//边框圆角
+							borderWidth:'10',
+							bgColor:'#68AD33',//背景颜色
+							display:'ALWAYS',//常显
+						},
+					},
+					{ 
+						id:5,
+						latitude:  39.121607,//纬度
+						longitude: 117.192895,//经度
+						iconPath: '',    //显示的图标    
+						width:20,   //宽
+						height:30,   //高
+						alpha:0.7,   //透明度
+						callout:{//自定义标记点上方的气泡窗口 点击有效  
+							content:'天津站',//文本
+							color:'#ffffff',//文字颜色
+							fontSize:14,//文本大小
+							borderRadius:15,//边框圆角
+							borderWidth:'8',
+							bgColor:'#D25555',//背景颜色
+							display:'ALWAYS',//常显
+						},
+					},
+					{
+						id:6,
+						latitude:  39.121807,//纬度
+						longitude: 117.243153,//经度
+						iconPath: '',    //显示的图标        
+						rotate:0,   // 旋转度数
+						width:20,   //宽
+						height:30,   //高
+						alpha:0.7,   //透明度
+						callout:{//自定义标记点上方的气泡窗口 点击有效  
+							content:'巴拉巴拉',//文本
+							color:'#ffffff',//文字颜色
+							fontSize:14,//文本大小
+							borderRadius:10,//边框圆角
+							borderWidth:'5',
+							bgColor:'#F0A010',//背景颜色
+							display:'ALWAYS',//常显
+						},
+					}
+				],
+            }
+        },
+        onLoad() {
+
+        },
+        computed:{
+		},
+        methods:{
+			//地图点击事件
+            markertap(e) {
+                console.log("===你点击了标记点===",e)
+                },
+            //地图点击事件
+            callouttap(e){
+                console.log('地图点击事件',e)
+        }
+        }
+    }
+</script>
---
+++ pages/recognition/recognition.vue
@@ -0,0 +1,99 @@
+<template>
+	<view class="body">
+		<view class="uni-margin-wrap">
+			<swiper class="swiper" circular indicator-dots="true" autoplay="true" interval="2500" duration="800">
+				<swiper-item>
+					<image class="item" src="../../static/pic.jpg" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image class="item" src="../../static/pic3.jpg" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image class="item" src="../../static/pic2.jpg" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image class="item" src="../../static/pic4.jfif" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<uni-search-bar radius="5" placeholder="文字搜索识别" clearButton="auto" cancelButton="auto" @confirm="search" />
+		<view class="btn">
+			<view class="item">
+				<view class="text">
+					拍照识别
+				</view>
+				<view class="text2">
+					search with picture
+				</view>
+				<image src="../../static/相机.png" mode="scaleToFill" style="width: 80px;height: 70px; position: absolute; right: 50px;bottom: 70px;"></image>
+			</view>
+			<view class="item">
+				<view class="text">
+					语音识别
+				</view>
+				<view class="text2">
+					search with voice
+				</view>
+				<image src="../../static/语音 (1).png" mode="scaleToFill" style="width: 70px;height: 73px; position: absolute; right: 60px;bottom: 70px;"></image>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		methods: {
+			search(res) {
+				uni.showToast({
+					title: '搜索：' + res.value,
+					icon: 'none'
+				})
+			},
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+.body{
+	// background-color: #40974c;
+}
+.uni-margin-wrap,.swiper,.item{
+	height: 200px;
+	width: 100%;
+}
+.uni-margin-wrap{
+	margin-bottom: 10px;
+}
+.btn{
+	display: flex;
+	justify-content: center;
+	gap:12px;
+	margin-top: 20px;
+	.item{
+		height: 280px;
+		width: 45%;
+		border-radius: 10px;
+		box-shadow: 0 0 5px #D4D7DE;
+		position: relative;
+	}
+}
+.text{
+	display: inline-block;
+	font-size: 23px;
+	color: #606266;
+	font-weight: 700;
+	margin-left: 20px;
+	margin-top: 20px;
+}
+.text2{
+	font-size: 15px;
+	color: #606266;
+	font-weight: 700;
+	margin-left: 20px;
+	margin-top: 5px;
+}
+</style>
