643dce3314b45206dcd4cb46d554e37159f3993b
Yowo_moran <zsj2805547052@163.com>
2023-10-11 23:43:11 +0800

    嘿嘿嘿

--- pages/recognition/pictureSearch/pictureSearch.vue
+++ pages/recognition/pictureSearch/pictureSearch.vue
@@ -2,24 +2,17 @@
 	<view>
 		<view class="result" v-if="show">
 			<view>
-				<image
-					:src="src" 
-					mode="scaleToFill"
-					style="width: 300px;height: 300px;border-radius: 20px;"
-					></image>
+				<image :src="src" mode="scaleToFill" style="width: 300px;height: 300px;border-radius: 20px;"></image>
 				<view class="text">
-					{{text}}
+					{{ text }}
 				</view>
 			</view>
 		</view>
 		<view class="noSearch" v-if="!show">
 			<view>
-				<image
-					src="../../../static/none.png" 
-					mode="aspectFill" 
-					></image>
+				<image src="../../../static/none.png" mode="aspectFill"></image>
 				<view class="text">
-					{{text}}
+					{{ text }}
 				</view>
 			</view>
 		</view>
@@ -32,41 +25,42 @@
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				show:false,
-				src:'../../../static/3.png',
-				text:'暂时木有内容呀～～'
-			}
-		},
-		methods: {
-			takePicture(){
-				uni.chooseImage({
-					count: 1, //默认9
-					sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
-					sourceType: ['camera'], //从相册选择
-					success: function (res) {
-						console.log(JSON.stringify(res.tempFilePaths));
-					}
-				});
-			}
-		},
-		onLaunch() {
-			
+export default {
+	data() {
+		return {
+			show: false,
+			src: '../../../static/3.png',
+			text: '暂时木有内容呀～～'
 		}
+	},
+	methods: {
+		takePicture() {
+			uni.chooseImage({
+				count: 1, //默认9
+				sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+				sourceType: ['camera'], //从相册选择
+				success: function (res) {
+					console.log(JSON.stringify(res.tempFilePaths));
+				}
+			});
+		}
+	},
+	onLaunch() {
+
 	}
+}
 </script>
 
 <style scoped lang="scss">
-.noSearch{
+.noSearch {
 	height: 50vh;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	margin-top: 50px;
 }
-.result{
+
+.result {
 	// background-color: firebrick;
 	height: 50vh;
 	margin-top: 70px;
@@ -74,19 +68,22 @@
 	justify-content: center;
 	align-items: center;
 }
-.text{
+
+.text {
 	text-align: center;
 	margin-top: 30px;
 	font-size: 20px;
 	color: #606266;
 }
-.btn{
+
+.btn {
 	// background-color: orange;
 	position: fixed;
 	bottom: 20vh;
 	height: 45px;
 	width: 100%;
-	.pic{
+
+	.pic {
 		text-align: center;
 		font-size: 20px;
 		width: 88%;
